060f6defdb22157adae0450a3ebc7fe3d1680133
pritambiswas00 <pritambiswas060@gmail.com>
2022-07-19 20:11:59 +0530

    second commit

--- index.js
+++ index.js
@@ -12,20 +12,6 @@ app.use(express.json());
 app.use(cors())
 app.use(express.static(path.join(__dirname,"public")));
 const server = http.createServer(app);
-// const io = require("socket.io")(server, {
-//       path: "/ws",
-//       cors : {
-//          origin: "*",
-//          methods: ["GET", "POST"]
-//       }
-// })
-// const newRouter = new Router(app,io, users);
-// newRouter.setupConnection();
-// newRouter.initialize();
-
-// app.get("/applocal/**", async (req, res)=> {
-       
-// })
 server.listen(PORT, ()=>{
        console.log("Server listening on port " + PORT);
 })
