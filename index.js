23cdec39f9d891c2c425b59ff956a0393d85598d
pritambiswas00 <pritambiswas060@gmail.com>
2022-07-20 12:05:59 +0530

    thrid

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+const { query } = require('express');
 var express = require('express');
 
 var app = express();
 var path = require('path');
 var PORT = 5000;
-var clientRoot = path.resolve(__dirname, '.', '', 'build');
-var indexPath = path.join(clientRoot, '', 'index.html');
+var clientRoot = path.resolve(__dirname, '.', '', 'public');
+console.log(clientRoot)
+var indexPath = path.join(clientRoot, 'agentportal', 'index.html');
 
 app.use(express.static(clientRoot));
 app.get('/*', function (req, res, next) {
+       console.log(req.hostname)
 	var q = "?";
 	for (var p in req.query) {
 		if (req.query.hasOwnProperty(p)) {
@@ -20,17 +23,17 @@ app.get('/*', function (req, res, next) {
 	res.sendFile(indexPath + ((q == "?") ? "" : q));
 });
 
-app.get("/**", async(req, res) => {
-       console.log(req.url)
-        req.url = req.url.replace(req.path, "/agentportal"+req.path); 
-        const response = await fetch(`http://localhost:5000${req.url}`, {
-              headers: {
-                     "Content-Type": ["text/javascript", "text/css", "application/json", ]
-              }
-        });
-        res.set(response.headers.get("content-type"));
-        response.body.pipe(res);
-})
+// app.get("/agentportal/static/**", async(req, res) => {
+//        console.log(req.url, "IN /****")
+        
+//         const response = await fetch(`http://localhost:5000${req.url}`, {
+//               headers: {
+//                      "Content-Type": ["text/javascript", "text/css", "application/json", ]
+//               }
+//         });
+//         res.set(response.headers.get("content-type"));
+//         response.body.pipe(res);
+// })
 
 var server = app.listen(PORT, function () {
 	var port = server.address().port;
