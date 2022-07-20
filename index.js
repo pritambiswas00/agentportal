3bc3a9b523dec7054b33b54db9c2d8cf96023cda
pritambiswas00 <pritambiswas060@gmail.com>
2022-07-20 10:10:30 +0530

    second commit

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const express = require('express');
-const app = express();
-const PORT = 5000;
-const users = [];
-const http = require('http');
-const path = require('path');
-const { Router } = require("./routes")
-const cors = require('cors');
+var express = require('express');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors())
-app.use(express.static(path.join(__dirname,"public")));
-const server = http.createServer(app);
-server.listen(PORT, ()=>{
-       console.log("Server listening on port " + PORT);
+var app = express();
+var path = require('path');
+var PORT = 5000;
+var clientRoot = path.resolve(__dirname, '.', '', 'build');
+var indexPath = path.join(clientRoot, '', 'index.html');
+
+app.use(express.static(clientRoot));
+app.get('/*', function (req, res, next) {
+	var q = "?";
+	for (var p in req.query) {
+		if (req.query.hasOwnProperty(p)) {
+			if (q != "?") {
+				q += "&";
+			}
+			q += (p + "=" + req.query[p]);
+		}
+	}
+	res.sendFile(indexPath + ((q == "?") ? "" : q));
+});
+
+app.get("/**", async(req, res) => {
+       console.log(req.url)
+        req.url = req.url.replace(req.path, "/agentportal"+req.path); 
+        const response = await fetch(`http://localhost:5000${req.url}`, {
+              headers: {
+                     "Content-Type": ["text/javascript", "text/css", "application/json", ]
+              }
+        });
+        res.set(response.headers.get("content-type"));
+        response.body.pipe(res);
 })
+
+var server = app.listen(PORT, function () {
+	var port = server.address().port;
+	console.log('App is listening on Http port:' + port);
+});
+
+
